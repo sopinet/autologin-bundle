85180c1c2da9f31d959fd5471c2d5c80930839fc
alejandro <aarrabal@sopinet.com>
2015-05-08 12:59:32 +0200

    Cleaner service dependency injection

--- Service/UrlHelper.php
+++ Service/UrlHelper.php
@@ -2,35 +2,41 @@
 
 namespace Sopinet\AutologinBundle\Service;
 
-class UrlHelper {			
-	function __construct($entityManager, $container, $router) {
-		$this->em = $entityManager;
-		$this->container = $container;
-		$this->router = $router;
-	}
+class UrlHelper {
+    function __construct($container)
+    {
+        $this->container = $container;
+    }
 
-	/**
-	 * Funcion para generar url autologin	 
-	 */
-	public function generateUrl($path, $user) {				
-				
-		if (null === $user->getConfirmationToken()) {
-			/** @var $tokenGenerator \FOS\UserBundle\Util\TokenGeneratorInterface */
-			$tokenGenerator = $this->container->get('fos_user.util.token_generator');
-			$token = $tokenGenerator->generateToken();
-			$user->setConfirmationToken($token);
-			
-			$this->em->persist($user);
-			$this->em->flush();
-		}
-		else{
-			$token = $user->getConfirmationToken();
-		}
+
+    /**
+     * Funcion para generar un autologin
+     * @param $path
+     * @param $user
+     * @return string
+     */
+    public function generateUrl($path, $user)
+    {
+        $this->em = $this->container->get('doctrine.orm.entity_manager');
+        $this->router= $this->container->get('router');
+
+        if (null === $user->getConfirmationToken()) {
+            /** @var $tokenGenerator \FOS\UserBundle\Util\TokenGeneratorInterface */
+            $tokenGenerator = $this->container->get('fos_user.util.token_generator');
+            $token = $tokenGenerator->generateToken();
+            $user->setConfirmationToken($token);
+
+            $this->em->persist($user);
+            $this->em->flush();
+        }
+        else{
+            $token = $user->getConfirmationToken();
+        }
 
         $domain = $this->container->getParameter('sopinet_autologin.domain');
 
         $url = $domain . $this->router->generate('auto_login', array('path' => $path, 'token' => $token));
 
         return $url;
-	}
+    }
 }
